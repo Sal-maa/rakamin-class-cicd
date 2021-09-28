2014d601cf5074a2b7830898b31c1b2da40da71b
Sal-maa <salmaa.matematika@gmail.com>
2021-09-28 20:49:27 +0700

    fixing main.go and go.mod

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rakamin
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/gofiber/fiber/v2 v2.19.0 // indirect
+	github.com/stretchr/testify v1.7.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":" + os.Getenv("PORT"))
+
 }
